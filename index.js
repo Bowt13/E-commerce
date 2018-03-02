9dd38325dc7abad9111c33e70e3707a574fdd16e
Bot13 <ruhtarduhd@gmail.com>
2018-03-02 10:58:34 +0100

    finish api

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
 var Sequelize = require('sequelize')
 var sequelize = new Sequelize('postgres://postgres:secret@localhost:5432/postgres')
@@ -14,6 +15,7 @@ app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE')
   next()
 })
+app.use(bodyParser.json())
 
 const Product = sequelize.define('product', {
   name: {
@@ -63,4 +65,56 @@ app.get('/products/:id', (req, res) => {
     res.status(500)
     res.json({ message: 'Oops! There was an error getting the products. Please try again' })
   })
+})
+
+app.post('/products', (req, res) => {
+  const product = req.body
+  console.log(product)
+  Product.create(product)
+  .then(entity => {
+    res.status(201)
+    res.json(entity)
+  })
+  .catch(err => {
+    console.error(err)
+    res.status(422)
+    res.json({ message: 'Oops! There was an error. Please try again' })
+  })
+})
+
+app.put('/products/:id', (req, res) => {
+  const productId = Number(req.params.id)
+  const updates = req.body
+  Product.findById(req.params.id)
+    .then(entity => {
+      return entity.update(updates)
+    })
+    .then(final => {
+      res.send(final)
+    })
+    .catch(error => {
+      res.status(500).send({
+        message: `Something went wrong`,
+        error
+      })
+    })
+})
+
+app.delete('/products/:id', (req, res) => {
+  const productId = Number(req.params.id)
+  Product.findById(req.params.id)
+    .then(entity => {
+      return entity.destroy()
+    })
+    .then(_ => {
+      res.send({
+        message: 'The product was deleted succesfully'
+      })
+    })
+    .catch(error => {
+      res.status(500).send({
+        message: `Something went wrong`,
+        error
+      })
+    })
 })
